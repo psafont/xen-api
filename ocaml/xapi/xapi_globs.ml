9d00370cf755ed0607adfd0d09a0c7563b8045e8
Pau Ruiz Safont <pau.ruizsafont@cloud.com>
2023-12-01 16:47:42 +0000

    maintenance: add undeclared dune libraries
    These can be detected by adding (implicit_transitive_deps false) to
    dune-project
    
    Signed-off-by: Pau Ruiz Safont <pau.ruizsafont@cloud.com>

--- ocaml/xapi/xapi_globs.ml
+++ ocaml/xapi/xapi_globs.ml
@@ -14,8 +14,7 @@
 
 (** A central location for settings related to xapi *)
 
-module String_plain = String (* For when we don't want the Xstringext version *)
-open Xapi_stdext_std.Xstringext
+module Stringext = Xapi_stdext_std.Xstringext.String
 module StringSet = Set.Make (String)
 
 module D = Debug.Make (struct let name = "xapi_globs" end)
@@ -1190,7 +1189,7 @@ let gen_list_option name desc of_string string_of opt =
   let parse s =
     opt := [] ;
     try
-      String.split_f String.isspace s
+      Stringext.split_f Stringext.isspace s
       |> List.iter (fun x -> opt := of_string x :: !opt)
     with e ->
       D.error "Unable to parse %s=%s (expected space-separated list) error: %s"
@@ -1290,7 +1289,7 @@ let other_options =
       (fun s -> s)
       disable_dbsync_for
   ; ( "xenopsd-queues"
-    , Arg.String (fun x -> xenopsd_queues := String.split ',' x)
+    , Arg.String (fun x -> xenopsd_queues := String.split_on_char ',' x)
     , (fun () -> String.concat "," !xenopsd_queues)
     , "list of xenopsd instances to manage"
     )
@@ -1372,7 +1371,8 @@ let other_options =
   ; ( "nvidia_multi_vgpu_enabled_driver_versions"
     , Arg.String
         (fun x ->
-          nvidia_multi_vgpu_enabled_driver_versions := String.split ',' x
+          nvidia_multi_vgpu_enabled_driver_versions :=
+            String.split_on_char ',' x
         )
     , (fun () -> String.concat "," !nvidia_multi_vgpu_enabled_driver_versions)
     , "list of nvidia host driver versions with multiple vGPU supported.\n\
