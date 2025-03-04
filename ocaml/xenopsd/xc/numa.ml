780e22919950d05c5596600e0494417d4cef97fc
Pau Ruiz Safont <pau.ruizsafont@cloud.com>
2025-03-04 16:34:21 +0000

    numa: add small test binary that prints memory changes

---
+++ ocaml/xenopsd/xc/numa.ml
@@ -0,0 +1,98 @@
+(* Monitoring loop that keeps track of per-numa-node memory changes, and prints
+   the change. Useful to see whether memory scrubbing is seen as used or free
+   memory by userspace *)
+open! Xenctrlext
+
+let stamp_tag : Mtime.span Logs.Tag.def =
+  Logs.Tag.def "stamp" ~doc:"Relative monotonic time stamp" Mtime.Span.pp
+
+let stamp c = Logs.Tag.(empty |> add stamp_tag (Mtime_clock.count c))
+
+let xc = get_handle ()
+
+let get_memory () =
+  let {memory; _} = numainfo xc in
+  memory
+
+let print_mem c mem =
+  for i = 0 to Array.length mem - 1 do
+    let {memfree; memsize} = mem.(i) in
+    Logs.app (fun m ->
+        m "\t%d: %Ld free out of %Ld" i memfree memsize ~tags:(stamp c)
+    )
+  done
+
+let diff c old cur =
+  let changed_yet = ref false in
+  for i = 0 to Int.min (Array.length old) (Array.length cur) - 1 do
+    let {memfree= a_free; _}, {memfree= b_free; _} = (old.(i), cur.(i)) in
+    if a_free <> b_free then (
+      if not !changed_yet then
+        changed_yet := true ;
+      let updown = if a_free > b_free then "🢆" else "🢅" in
+      Logs.app (fun m -> m "\t%d: %Ld %s" i b_free updown ~tags:(stamp (c ())))
+    )
+  done ;
+  !changed_yet
+
+let reporter ppf =
+  let report _src level ~over k msgf =
+    let k _ = over () ; k () in
+    let with_stamp h tags k ppf fmt =
+      let stamp =
+        match tags with
+        | None ->
+            None
+        | Some tags ->
+            Logs.Tag.find stamp_tag tags
+      in
+      let span_pp s =
+        match s with
+        | None ->
+            "0ns"
+        | Some s ->
+            Fmt.to_to_string Mtime.Span.pp s
+      in
+      Format.kfprintf k ppf
+        ("%a[%s] @[" ^^ fmt ^^ "@]@.")
+        Logs.pp_header (level, h) (span_pp stamp)
+    in
+    msgf @@ fun ?header ?tags fmt -> with_stamp header tags k ppf fmt
+  in
+  {Logs.report}
+
+let memory_changes () =
+  let max_time = Mtime.Span.(7 * s) in
+
+  let memory = get_memory () in
+  let c = Mtime_clock.counter () in
+  print_mem c memory ;
+  let rec loop since_started since_changed previous =
+    let current = get_memory () in
+
+    let since_started = ref since_started in
+    let timer () =
+      let last_changed = Mtime_clock.count since_changed in
+      if Mtime.Span.is_longer last_changed ~than:max_time then
+        since_started := Mtime_clock.counter () ;
+      !since_started
+    in
+
+    let changed = diff timer previous current in
+
+    let since_changed =
+      if changed then
+        Mtime_clock.counter ()
+      else
+        !since_started
+    in
+    Unix.sleepf 0.01 ;
+    loop !since_started since_changed current
+  in
+  loop c c memory
+
+let () =
+  Logs.set_reporter (reporter Format.std_formatter) ;
+  Logs.set_level (Some Logs.Info) ;
+
+  ignore (Thread.create memory_changes () : Thread.t)
